b5b3daabe991038bc56837cf710ffd763c063831
logunlaja26 <logunlaja26@gmail.com>
2021-07-13 10:32:28 -0500

    added beer entity with JPA entity config

--- src/main/java/com/lyomann/msscbrewery/domain/Beer.java
+++ src/main/java/com/lyomann/msscbrewery/domain/Beer.java
@@ -1,4 +1,46 @@
 package com.lyomann.msscbrewery.domain;
 
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar",updatable = false,nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastmodifiedDate;
+    private String beerName;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
 }
